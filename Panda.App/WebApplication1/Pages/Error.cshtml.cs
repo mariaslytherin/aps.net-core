e0489eef843a401b1288dad137e3e1080599e2bc
Mariya Pirinova <Mariya Pirinova@MPIRINOVA>
2019-06-24 16:04:56 +0300

    Added home controller, refactoring

--- Panda.App/WebApplication1/Pages/Error.cshtml.cs
+++ Panda.App/WebApplication1/Pages/Error.cshtml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Panda.App.Pages
+namespace WebApplication1.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
