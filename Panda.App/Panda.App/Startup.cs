8b04fc1da5572e01d10de59468ee337308b9898e
Mariya Pirinova <Mariya Pirinova@MPIRINOVA>
2019-06-26 16:19:16 +0300

    Added css files and made panda.data

--- Panda.App/Panda.App/Startup.cs
+++ Panda.App/Panda.App/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Panda.Data;
 
 namespace Panda.App
 {
@@ -17,6 +19,9 @@ namespace Panda.App
 
         public void ConfigureServices(IServiceCollection services)
         {
+              services.AddDbContext<PandaDbContext>(options =>
+                options.UseSqlServer(this.Configuration[""]));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
