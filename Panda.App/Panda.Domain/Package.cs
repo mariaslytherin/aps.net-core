b5123901cd4979d49fb78354350d3ee2619a3764
mariaslytherin <nirvana4ever>
2019-07-02 00:02:12 +0300

    Added properties to the entities

---
+++ Panda.App/Panda.Domain/Package.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Panda.Domain
+{
+    public class Package
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Weight { get; set; }
+
+        public string ShoppingAdress { get; set; }
+
+        public PackageStatus Status { get; set; }
+
+        public DateTime? EstimatedDeliveryDate { get; set; }
+
+        public string RecipientId { get; set; }
+
+        public PandaUser Recipient { get; set; }
+
+        public string ReceiptId { get; set; }
+
+        public Receipt Receipt { get; set; }
+    }
+}
